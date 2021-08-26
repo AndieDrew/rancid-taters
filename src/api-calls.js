362d7f539fe01adaf8bbae212cfce8b22aa26e88
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-08-26 16:16:11 -0400

    Remove fetch method used to test response

--- src/api-calls.js
+++ src/api-calls.js
@@ -12,3 +12,8 @@ export const getMovie = ( id ) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(response => response.json())
 }
+
+export const getQuote = ( ) => {
+  return fetch('https://animechan.vercel.app/api/random')
+     .then(response => response.json())
+}
